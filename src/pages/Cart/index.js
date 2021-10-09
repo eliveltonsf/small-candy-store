3879a6481c0524c49d3775f3ab14f58bb2da3a22
eliveltonsf <elivelton.seven@gmail.com>
2021-10-09 14:44:07 -0300

    feat: creating Cart page

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,111 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { formatPrice } from '../../util/format';
+
+import * as CartActions from '../../store/modules/cart/actions';
+
+import { Container, ProductTable, FreeShipping, Total } from './styles';
+
+export default function Cart() {
+  const total = useSelector((state) => {
+    const valueTotal = state.cart.reduce((totalSum, product) => {
+      return totalSum + (product.sellingPrice / 100) * product.amount;
+    }, 0);
+
+    return {
+      valorTotal: valueTotal,
+      total: formatPrice(valueTotal),
+    };
+  });
+
+  const cart = useSelector((state) =>
+    state.cart.map((product) => ({
+      ...product,
+      subtotal: formatPrice((product.sellingPrice / 100) * product.amount),
+    })),
+  );
+
+  const dispatch = useDispatch();
+
+  function increment(product) {
+    dispatch(CartActions.updateAmount(product.id, product.amount + 1));
+  }
+
+  function decrement(product) {
+    dispatch(CartActions.updateAmount(product.id, product.amount - 1));
+  }
+
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th colSpan="2">PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((product) => (
+            <tr>
+              <td>
+                <img src={product.imageUrl} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.name}</strong>
+                <small>{product.priceFormatted}</small>
+                <span>{product.sellingPriceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button" onClick={() => decrement(product)}>
+                    <MdRemoveCircleOutline size={20} color="#70402b" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button" onClick={() => increment(product)}>
+                    <MdAddCircleOutline size={20} color="#70402b" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>{product.subtotal}</strong>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch(CartActions.removeFromCart(product.id))
+                  }
+                >
+                  <MdDelete size={20} color="#70402b" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        {total.valorTotal >= 10 && (
+          <FreeShipping>
+            <span>Parabéns, sua compra tem frete grátis !</span>
+          </FreeShipping>
+        )}
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>{total.total}</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,112 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  padding: 30px;
+  background: #fff;
+  border-radius: 4px;
+
+  footer {
+    border-top: 1px solid #70402b;
+    padding-top: 20px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button {
+      background: #70402b;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      padding: 12px 20px;
+      font-weight: bold;
+      text-transform: uppercase;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.05, '#70402b')};
+      }
+    }
+  }
+`;
+
+export const ProductTable = styled.table`
+  width: 100%;
+
+  thead th {
+    color: #532c1b;
+    text-align: left;
+    padding: 12px;
+  }
+
+  tbody td {
+    padding: 12px;
+  }
+
+  img {
+    width: 80px;
+    height: 100px;
+  }
+
+  strong {
+    color: #70402b;
+    display: block;
+  }
+
+  small {
+    color: #888;
+  }
+
+  span {
+    display: block;
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #666;
+      padding: 6px;
+      width: 50px;
+    }
+  }
+
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
+`;
+
+export const FreeShipping = styled.div`
+  display: flex;
+  background: #78cea5;
+  border-radius: 10px;
+  padding: 10px;
+
+  span {
+    color: #fff;
+  }
+`;
+
+export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+
+  span {
+    color: #532c1b;
+    font-weight: bold;
+  }
+
+  strong {
+    font-size: 28px;
+    margin-left: 5px;
+  }
+`;
