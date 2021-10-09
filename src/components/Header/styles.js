52f5a174d2859570d6d2725d564c32a758e71053
eliveltonsf <elivelton.seven@gmail.com>
2021-10-09 14:41:42 -0300

    feat: add header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,43 @@
+import Styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Container = Styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+
+  img {
+    height: 100px;
+  }
+`;
+
+export const Cart = Styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+    color: #999
+
+    strong {
+      display: block;
+      color: #532c1b;
+    }
+
+    span {
+      font-size: 12px;
+      color: #FFF;
+      background: #532c1b;
+      padding: 2px 6px;
+      border-radius: 10px;
+    }
+  }
+`;
